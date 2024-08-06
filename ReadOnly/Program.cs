47c6f84a198f60fc0dd818319681cba78633079c
Vinicius Duarte <146869828+ViniCellist@users.noreply.github.com>
2024-08-06 17:58:40 -0300

    using ReadOnlyCollection

---
+++ ReadOnly/Program.cs
@@ -0,0 +1,24 @@
+﻿using A23ListasDeObjetos;
+using ReadOnly;
+
+namespace A24ListaSomenteLeitura
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Curso csharpColecoes = new Curso("C# Collections", "Vinicius Duarte");
+            csharpColecoes.Adiciona(new Aula("Trabalhando com Listas", 21));
+            Imprimir(csharpColecoes.Aulas);
+        }
+
+        private static void Imprimir(IList<Aula> aulas)
+        {
+            Console.Clear();
+            foreach (var aula in aulas)
+            {
+                Console.WriteLine(aula);
+            }
+        }
+    }
+}
