e79362c288cad1eda5296e1f24b8e82d55b9bfa5
Vinicius Duarte <146869828+ViniCellist@users.noreply.github.com>
2024-08-06 17:32:11 -0300

    using compareTo()

--- Object/obj/Debug/net8.0/Object.AssemblyInfo.cs
+++ Object/obj/Debug/net8.0/Object.AssemblyInfo.cs
@@ -11,12 +11,12 @@
 using System;
 using System.Reflection;
 
-[assembly: System.Reflection.AssemblyCompanyAttribute("Collections")]
+[assembly: System.Reflection.AssemblyCompanyAttribute("Object")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
 [assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0a2bbdfd9c96766766438d8f56ff333487c7a922")]
-[assembly: System.Reflection.AssemblyProductAttribute("Collections")]
-[assembly: System.Reflection.AssemblyTitleAttribute("Collections")]
+[assembly: System.Reflection.AssemblyProductAttribute("Object")]
+[assembly: System.Reflection.AssemblyTitleAttribute("Object")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
 
 // Gerado pela classe WriteCodeFragment do MSBuild.
