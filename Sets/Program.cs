26dd0941ec30d00058bc1b28945dff8d1b300a75
Vinicius Duarte <146869828+ViniCellist@users.noreply.github.com>
2024-08-07 14:27:05 -0300

    feat Sets

---
+++ Sets/Program.cs
@@ -0,0 +1,35 @@
+﻿namespace A31OPoderDosSets
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Sets = Conjuntos
+            //1 - Não permite duplicidade
+            //2 - Os elementos não tem ordem específica
+
+            //declarando set de alunos
+
+            ISet<string> alunos = new HashSet<string>();
+
+            alunos.Add("Vanessa Tonini");
+            alunos.Add("Ana Losnak");
+            alunos.Add("Rafael Nercessian");
+
+            Console.WriteLine(alunos);
+            Console.WriteLine(string.Join(",", alunos));
+
+            alunos.Add("Priscila Stuani");
+            alunos.Add("Rafael Rollo");
+            alunos.Add("Fabio Gushiken");
+            Console.WriteLine(string.Join(",", alunos));
+
+            alunos.Remove("Ana Losnak");
+            alunos.Add("Marcelo Oliveira");
+            Console.WriteLine(string.Join(",", alunos));
+
+            alunos.Add("Fabio Gushiken");
+            Console.WriteLine(string.Join(",", alunos));
+        }
+    }
+}
--- Sets/obj/Debug/net8.0/Sets.AssemblyInfo.cs
+++ Sets/obj/Debug/net8.0/Sets.AssemblyInfo.cs
@@ -11,12 +11,12 @@
 using System;
 using System.Reflection;
 
-[assembly: System.Reflection.AssemblyCompanyAttribute("Order")]
+[assembly: System.Reflection.AssemblyCompanyAttribute("Sets")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
 [assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+cac9f93d12f65db94331731a20789b021fb51b96")]
-[assembly: System.Reflection.AssemblyProductAttribute("Order")]
-[assembly: System.Reflection.AssemblyTitleAttribute("Order")]
+[assembly: System.Reflection.AssemblyProductAttribute("Sets")]
+[assembly: System.Reflection.AssemblyTitleAttribute("Sets")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
 
 // Gerado pela classe WriteCodeFragment do MSBuild.
